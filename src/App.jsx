e18b9e2926c370bb16d499d3f5660800d848a1f9
Güzide Yağmur <guzideyagmur@hotmail.com>
2025-03-12 18:49:59 +0300

    movie sayfası yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import HomePage from "./components/HomePage";
 import Navbar from "./components/navbar";
+import MoviesPage from "./components/MoviesPage";
+
 
 
 export default function App() {
-    return (
-        <div className="bg-[#10141E]">
-            <Navbar/>  
-            <HomePage />
+
+    const [page, setPage]= useState(window.location.pathname);
+        useEffect(() => {
+            const onPopState = () => setPage(window.location.pathname);
+            window.addEventListener("popstate", onPopState);
+    
+            return () => window.removeEventListener("popstate", onPopState);
+        }, []);
+    
+         return (
+            <>
+             <div className="bg-[#10141E]">
+                 <Navbar setPage={setPage}/>
+                {page === "/homepage" && <HomePage />}
+                {page === "/movies" && <MoviesPage />}
+                {page === "/series" && <Series />}
+                {page === "/bookmark" && <BookMark />}
+            
         </div>
-    );
+           
+            </>
+        );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,30 +1,10 @@
 import { useEffect, useState } from "react";
 import { navigate } from "./helper";
 import HomePage from "./HomePage";
+import MoviesPage from "./MoviesPage";
 
-export default function Navbar(){
-    const [page, setPage]= useState(window.location.pathname);
-    useEffect(() => {
-        const onPopState = () => setPage(window.location.pathname);
-        window.addEventListener("popstate", onPopState);
-
-        return () => window.removeEventListener("popstate", onPopState);
-    }, []);
-
-     return (
-        <>
-        <Header setPage={setPage}/>
-            {page === "/homepage" && <Home />}
-            {page === "/movies" && <Movies />}
-            {page === "/series" && <Series />}
-            {page === "/bookmark" && <BookMark />}
-        </>
-    );
-
-}
-
-function Header({setPage}){
-        return(
+export default function Navbar({setPage}){
+         return(
         <div className="flex gap-[80px] justify-between p-[16px] items-center bg-[#161D2F] hover:text-white">
             <a href=""> <img src="./redicon.svg" alt="" /></a>
             
@@ -40,9 +20,8 @@ function Header({setPage}){
         </div>
 
     );
+    
+
 }
 
 
-function Home(){
-    return <HomePage/>
-}
