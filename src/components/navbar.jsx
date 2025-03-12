94ceb8c11310d9ff37a417a4b06e85a212390367
Güzide Yağmur <guzideyagmur@hotmail.com>
2025-03-12 13:51:52 +0300

    navbar yapılıyor

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+export default function Navbar(){
+
+
+
+    return(
+        <div>
+            <a href=""> <img src="./redicon.svg" alt="" /></a>
+            
+            <div>
+                 <a href=""> <img src="./homepage-icon.svg" alt="" /></a>
+                 <a href=""> <img src="./movie-icon.svg" alt="" /></a>
+                 <a href=""> <img src="./series-icon.svg" alt="" /></a>
+            
+                
+            </div>
+        </div>
+
+    )
+}
